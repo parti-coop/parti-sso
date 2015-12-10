86f51b26c1adb89bea63b278fdebeb58bb87ae89
dali <dali@ufofactory.org>
2015-12-10 23:53:09 +0900

    username should be email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :password, length: { minimum: 6 }
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :username,
+    presence: true,
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
   has_secure_password
 end
