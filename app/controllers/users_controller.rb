f1be66768bec36a9f4d3603ff489f6f8b29dba20
dali <dali@ufofactory.org>
2015-12-10 21:56:18 +0900

    sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  include CASino::SessionsHelper
+
+  before_action :ensure_signed_in,  only: [:edit, :update, :destroy]
+  before_action :correct_user,      only: [:edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      data = { authenticator: 'ActiveRecord', user_data: { username:  @user.username } }
+      sign_in(data)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password,
+                                 :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(casino.login_path) unless current_user == @user
+  end
+
+  def sessions_path
+    casino.sessions_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount CASino::Engine => '/', :as => 'casino'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
