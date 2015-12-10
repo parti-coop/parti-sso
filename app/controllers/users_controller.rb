bb6a842717b7c6342c03f53d613ff3c2f333a95a
dali <dali@ufofactory.org>
2015-12-11 00:56:07 +0900

    If a user signed in, and try to sign up, redirect back.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
   before_action :correct_user,      only: [:edit, :update, :destroy]
 
   def new
+    redirect_to(:back) if signed_in?
+
     @user = User.new
   end
 
